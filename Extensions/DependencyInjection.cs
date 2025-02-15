af952bee6dc49af96cff2d94de5e5facc5716a8e
rudepiece <reubenkisv@gmail.com>
2025-02-15 18:21:25 +0200

    Added DI back

---
+++ Extensions/DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using investec.Models;
+using investec.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Zebra.NET.Extensions;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInvestecOpenBanking(IServiceCollection services, Authenticator options)
+    {
+        services.AddSingleton(new BankingService(options));
+        return services;
+    }
+}
